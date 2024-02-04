4289164d7fbeca4c0c0420127393ff5b796bb4a0
JonasNon <officialjonasn@gmail.com>
2024-02-04 01:17:36 -0600

    made into wirefram on black background that follows cursor

--- scripts.js
+++ scripts.js
@@ -10,7 +10,6 @@ const holder = document.getElementById("square-holder")
 // para.style.background = "green";
 
 
-
 let divList = []
 let squareCount = 100
 let avaliableColors = ['red','blue','orange','purple','green','cyan','black','yellow','pink']
@@ -23,29 +22,77 @@ for (let i = 0; i < squareCount; i++) {
   newDiv.style.visibility = "hidden";
   holder.appendChild(newDiv)
   divList.push(newDiv)
+
+  let rect = newDiv.getBoundingClientRect();
+  let currentLeft = rect.left
+  let currentTop = rect.top
+  
+  
+  
+  newDiv.style.left = currentLeft
+  newDiv.style.top = currentTop
+  console.log(currentLeft)
+  // console.log(currentTop)
+
 }
 
 const testLog = () => {
-  console.log(divList)
+  // console.log(divList)
+
 }
 
 const onMouseMove = (e) =>{
   for (let i = 0; i < squareCount; i++) {
-    let currentDiv = divList[i]
+    let currentDiv = divList[i];
+    // console.log(currentDiv.style.border)
     
+    // console.log(rect)
+    // let rectLeft = rect.left
+    // let rectTop = rect.top
+    // console.log(rectTop)
+
+
+
+
+
     let divColor = avaliableColors[Math.floor(Math.random()*avaliableColors.length)]
-    currentDiv.style.background = divColor
+    
+    // currentDiv.style.background = divColor
+    // COMMENT OUT ABOVE LINE FOR VAST CHANGE
     divColor = avaliableColors[Math.floor(Math.random()*avaliableColors.length)]
-    currentDiv.style.border = divColor
+    currentDiv.style.outlineColor = divColor
+
+    let currentLeft = currentDiv.style.left
+    let currentTop = currentDiv.style.top
+
+
+    console.log(e.offsetX)
+    currentDiv.style.left = e.offsetX + 'px';
+    currentDiv.style.top = e.offsetY + 'px';
+    //COMMENT OUT ABOVE 3 LINES FOR STATIC MOVEMENT
+
+    currentDiv.style.position = "relative"
+
+
   }
+  // for (let i = 0; i < squareCount; i++) {
+
+  //   currentDiv = divList[i];
+    
+  // }
 }
 document.addEventListener('mousemove', onMouseMove);
 
 
 const unHide = () => {
+  document.body.style.cursor = 'none';
+
   for (let i = 0; i < squareCount; i++) {
     let currentDiv = divList[i]
     currentDiv.style.visibility = "visible";
+    
+
+    
   }
   document.getElementById("yes").style.visibility = "hidden";
   document.getElementById("startText").style.visibility = "hidden";
