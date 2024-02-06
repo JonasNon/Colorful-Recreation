14b744579907932cea5c0fff1bb5b6ae9827e825
JonasNon <officialjonasn@gmail.com>
2024-02-06 17:23:15 -0600

    added color options

--- scripts.js
+++ scripts.js
@@ -12,34 +12,52 @@ const holder = document.getElementById("square-holder")
 
 let divList = []
 let squareCount = 100
+let avaliableColorsArray = [
+['red','blue','orange','purple','green','cyan','yellow','pink','black'],
+['#618264','#79AC78','#B0D9B1','#D0E7D2'], //green-ish
+['#EEF5FF','#B4D4FF','#86B6F6','#176B87'], //blue-ish
+['#711DB0','#C21292','#EF4040','#FFA732'], //purple-ish
+['red','blue','orange','purple','green','cyan','yellow','pink','black','black','black','black','black','black','black','black','black','black','black','black','black'],
+
+]
 let avaliableColors = ['red','blue','orange','purple','green','cyan','yellow','pink','black']
+
 //change above array to determine colors
+
 
 let colorfulness = false
 let static = true
 let inMenu = true
+const menuItems = document.getElementsByClassName("menuItem");
+
 
 
 //get screen length
-for (let i = 0; i < squareCount; i++) {
-  let newDiv = document.createElement("div");
-  // newDiv.style.border = "white"
-  newDiv.style.visibility = "hidden";
-  holder.appendChild(newDiv)
-  divList.push(newDiv)
-
-  let rect = newDiv.getBoundingClientRect();
-  let currentLeft = rect.left
-  let currentTop = rect.top
+divCreator = () => {
+  for (let i = 0; i < squareCount; i++) {
+    let newDiv = document.createElement("div");
+    // newDiv.style.border = "white"
+    newDiv.style.visibility = "hidden";
+    holder.appendChild(newDiv)
+    divList.push(newDiv)
   
+    let rect = newDiv.getBoundingClientRect();
+    let currentLeft = rect.left
+    let currentTop = rect.top
+    
+    
+    
+    newDiv.style.left = currentLeft
+    newDiv.style.top = currentTop
+    // console.log(currentLeft)
+    // console.log(currentTop)
   
-  
-  newDiv.style.left = currentLeft
-  newDiv.style.top = currentTop
-  console.log(currentLeft)
-  // console.log(currentTop)
+  }
 
 }
+divCreator();
+//put this for loop in the above funstion when possible
+
 
 const testLog = () => {
   // console.log(divList)
@@ -111,14 +129,14 @@ const unHide = (setting) => {
 
   inMenu = false;
 
-  document.getElementById("startText").style.visibility = "hidden";
-  document.getElementById("little").style.visibility = "hidden";
-  document.getElementById("yes").style.visibility = "hidden";
-  document.getElementById("dynamic").style.visibility = "hidden";
-  // document.getElementById("static").style.visibility = "hidden";
-  document.getElementById("move").style.visibility = "hidden";
-  // document.getElementById("dontMove").style.visibility = "hidden";
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].style.visibility = "hidden";
+  }
+
+
+  // divCreator();
 }
+
 
 const changeStatic = (option) => {
   static = option
@@ -131,4 +149,12 @@ const changeStatic = (option) => {
     //   console.log('HERE')
     // }
   }
+  
+}
+
+
+
+const changeColor = (color) => {
+  avaliableColors = avaliableColorsArray[color]
+  
 }
